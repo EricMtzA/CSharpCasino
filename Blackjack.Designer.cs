589ecf7dc51459290ff9cca6aafe8345456e0b0d
EricMtzA <ericmartinez974@gmail.com>
2021-05-08 17:49:28 -0700

    Blackjack finished

--- Blackjack.Designer.cs
+++ Blackjack.Designer.cs
@@ -32,6 +32,18 @@ namespace CSharpCasino
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Blackjack));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pHand1 = new System.Windows.Forms.Label();
+            this.pHand2 = new System.Windows.Forms.Label();
+            this.dHand1 = new System.Windows.Forms.Label();
+            this.dHand2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pHand3 = new System.Windows.Forms.Label();
+            this.dHand3 = new System.Windows.Forms.Label();
+            this.pHand4 = new System.Windows.Forms.Label();
+            this.dHand4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -49,20 +61,162 @@ namespace CSharpCasino
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Unispace", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.button2.Location = new System.Drawing.Point(218, 150);
+            this.button2.ForeColor = System.Drawing.Color.Green;
+            this.button2.Location = new System.Drawing.Point(240, 218);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(81, 40);
+            this.button2.Size = new System.Drawing.Size(73, 27);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Hit!";
+            this.button2.Text = "Bet";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pHand1
+            // 
+            this.pHand1.AutoSize = true;
+            this.pHand1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pHand1.ForeColor = System.Drawing.Color.Green;
+            this.pHand1.Location = new System.Drawing.Point(145, 50);
+            this.pHand1.Name = "pHand1";
+            this.pHand1.Size = new System.Drawing.Size(0, 15);
+            this.pHand1.TabIndex = 2;
+            // 
+            // pHand2
+            // 
+            this.pHand2.AutoSize = true;
+            this.pHand2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pHand2.ForeColor = System.Drawing.Color.Green;
+            this.pHand2.Location = new System.Drawing.Point(145, 84);
+            this.pHand2.Name = "pHand2";
+            this.pHand2.Size = new System.Drawing.Size(0, 17);
+            this.pHand2.TabIndex = 3;
+            // 
+            // dHand1
+            // 
+            this.dHand1.AccessibleName = "dHand1";
+            this.dHand1.AutoSize = true;
+            this.dHand1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dHand1.ForeColor = System.Drawing.Color.Green;
+            this.dHand1.Location = new System.Drawing.Point(359, 50);
+            this.dHand1.Name = "dHand1";
+            this.dHand1.Size = new System.Drawing.Size(0, 17);
+            this.dHand1.TabIndex = 4;
+            // 
+            // dHand2
+            // 
+            this.dHand2.AccessibleName = "dHand1";
+            this.dHand2.AutoSize = true;
+            this.dHand2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dHand2.ForeColor = System.Drawing.Color.Green;
+            this.dHand2.Location = new System.Drawing.Point(359, 84);
+            this.dHand2.Name = "dHand2";
+            this.dHand2.Size = new System.Drawing.Size(0, 17);
+            this.dHand2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Unispace", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.Green;
+            this.button3.Location = new System.Drawing.Point(240, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 28);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Hit!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Unispace", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.Color.Green;
+            this.button4.Location = new System.Drawing.Point(240, 285);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 26);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Fold";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pHand3
+            // 
+            this.pHand3.AutoSize = true;
+            this.pHand3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pHand3.ForeColor = System.Drawing.Color.Green;
+            this.pHand3.Location = new System.Drawing.Point(145, 120);
+            this.pHand3.Name = "pHand3";
+            this.pHand3.Size = new System.Drawing.Size(0, 17);
+            this.pHand3.TabIndex = 8;
+            //is.pHand3.Click += new System.EventHandler(this.pHand3_Click);
+            // 
+            // dHand3
+            // 
+            this.dHand3.AutoSize = true;
+            this.dHand3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dHand3.ForeColor = System.Drawing.Color.Green;
+            this.dHand3.Location = new System.Drawing.Point(359, 120);
+            this.dHand3.Name = "dHand3";
+            this.dHand3.Size = new System.Drawing.Size(0, 17);
+            this.dHand3.TabIndex = 9;
+            // 
+            // pHand4
+            // 
+            this.pHand4.AutoSize = true;
+            this.pHand4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pHand4.ForeColor = System.Drawing.Color.Green;
+            this.pHand4.Location = new System.Drawing.Point(145, 155);
+            this.pHand4.Name = "pHand4";
+            this.pHand4.Size = new System.Drawing.Size(0, 17);
+            this.pHand4.TabIndex = 10;
+            //is.pHand4.Click += new System.EventHandler(this.pHand4_Click);
+            // 
+            // dHand4
+            // 
+            this.dHand4.AutoSize = true;
+            this.dHand4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dHand4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.dHand4.Location = new System.Drawing.Point(359, 155);
+            this.dHand4.Name = "dHand4";
+            this.dHand4.Size = new System.Drawing.Size(0, 17);
+            this.dHand4.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Unispace", 8.999999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(119, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 14);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Player\'s Cards";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Unispace", 8.999999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(325, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 14);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Dealer\'s Cards";
             // 
             // Blackjack
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dHand4);
+            this.Controls.Add(this.pHand4);
+            this.Controls.Add(this.dHand3);
+            this.Controls.Add(this.pHand3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dHand2);
+            this.Controls.Add(this.dHand1);
+            this.Controls.Add(this.pHand2);
+            this.Controls.Add(this.pHand1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -70,6 +224,7 @@ namespace CSharpCasino
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Blackjack";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,5 +232,17 @@ namespace CSharpCasino
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label pHand1;
+        private System.Windows.Forms.Label pHand2;
+        private System.Windows.Forms.Label dHand1;
+        private System.Windows.Forms.Label dHand2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label pHand3;
+        private System.Windows.Forms.Label dHand3;
+        private System.Windows.Forms.Label pHand4;
+        private System.Windows.Forms.Label dHand4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Blackjack.cs
+++ Blackjack.cs
@@ -16,6 +16,8 @@ namespace CSharpCasino
         public Blackjack()
         {
             InitializeComponent();
+            button3.Enabled = false;
+            button4.Enabled = false;
         }
 
         // Back button on the blackjack window
@@ -27,19 +29,235 @@ namespace CSharpCasino
             this.Hide();
         }
 
-        // Hit button to get thew new card
-        private void button2_Click(object sender, EventArgs e)
+        int bal = Global.balance;
+
+        int pHandOne;
+        int dHandOne;
+        int pHandTwo;
+        int dHandTwo;
+        int pHandOld;
+        int dHandOld;
+        int pHandNext;
+        int dHandNext;
+
+        int gambleAmount;
+
+        int clicks = 0;
+
+        // method to change balance when player wins
+        private int gambleWin(int gambleAmount)
         {
-         string result = Interaction.InputBox("Please place your bet!", "Gamble Amount", "$$$", -1, -1);
-           
-            if(Int32.TryParse(result, out int resultInt))
+            bal = bal - gambleAmount;
+            int winnings = gambleAmount * 2;
+
+            bal = bal + winnings;
+
+            return bal;
+        }
+
+        // method to change balance when player loses
+        private int gambleLose(int gambleAmount)
+        {
+            bal = bal - gambleAmount;
+
+            return bal;
+        }
+
+        // method to get a random card number
+        private int cardNumber()
+        {
+            Random rand = new Random();
+            int card = rand.Next(1, 14);
+
+            return card;
+        }
+
+        // compare the player and dealers hands, determines if player wins, loses or can continue to hit
+        private void compareHands(int pHand, int dHand)
+        {
+            if(pHand == 21 || dHand > 21)
             {
-                Console.WriteLine(resultInt);
-            }
+                DialogResult res = MessageBox.Show("You won!", "Winner!!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                if (res == DialogResult.OK)
+                {
+                    bal = gambleWin(gambleAmount);
+                    MessageBox.Show("New balance: " + bal, "Balance");
+
+                    resetGame();
+
+                }
+
+            } 
+            else if(dHand == 21 || pHand > 21)
+            {
+                DialogResult res = MessageBox.Show("You lost!", "Loser!!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                if (res == DialogResult.OK)
+                {
+                    bal = gambleLose(gambleAmount);
+                    MessageBox.Show("New balance: " + bal,"Balance");
+
+                    resetGame();
+
+                }
+            } 
             else
             {
-                Console.WriteLine("Could not parse!");
+                DialogResult res = MessageBox.Show("Your Hand: " + pHand + "\n Dealer's Hand: " + dHand, "Current Hands!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                if (res == DialogResult.OK)
+                {
+                    MessageBox.Show("Good luck!", "Hit or Fold?");
+
+                }
+            }
+
+        }
+
+        // method to reset the black jack game
+        private void resetGame()
+        {
+            // enable bet button, disable hit and fold button
+            button2.Enabled = true;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+            //reset clicks
+            clicks = 0;
+
+            //clear the labels
+            pHand1.Text = " ";
+            pHand2.Text = " ";
+            pHand3.Text = " ";
+            pHand4.Text = " ";
+
+
+            dHand1.Text = " ";
+            dHand2.Text = " ";
+            dHand3.Text = " ";
+            dHand4.Text = " ";
+        }
+
+
+        // Bet button to place bet and enable hit and fold buton
+        private void button2_Click(object sender, EventArgs e)
+        {
+                string result = Interaction.InputBox("Please place your bet!", "Gamble Amount", "$$$", -1, -1);
+
+                if (Int32.TryParse(result, out int resultInt))
+                {
+                // enable Hit Button and Fold Button
+                    button2.Enabled = false;
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+
+                    gambleAmount = resultInt;
+                    Console.WriteLine(resultInt);
+                }
+                else
+                {
+                    Console.WriteLine("Could not parse!");
+                }
+            
+        }
+
+
+
+        // Hit button to get player cards
+        private void button3_Click(object sender, EventArgs e)
+        {
+            switch (clicks)
+            {
+                case 0:
+                    // get random number
+                    pHandOne = cardNumber();
+                    dHandOne = cardNumber();
+
+                    // increment clicks
+                    clicks++;
+
+                    // set text of labels to corresponding number
+                    pHand1.Text = pHandOne.ToString();
+                    dHand1.Text = dHandOne.ToString();
+
+                    break;
+
+                case 1:
+
+                    // get random number
+                    pHandTwo = cardNumber();
+                    dHandTwo= cardNumber();
+
+                    // increment clicks
+                    clicks++;
+
+                    // set text of labels to corresponding number
+                    pHand2.Text = pHandTwo.ToString();
+                    dHand2.Text = dHandTwo.ToString();
+
+                    
+                    pHandOld = pHandOne + pHandTwo;
+                    dHandOld = dHandOne + dHandTwo;
+
+                    compareHands(pHandOld, dHandOld);
+
+                    break;
+
+                case 2:
+                    // get random number
+                    pHandNext = cardNumber();
+                    dHandNext = cardNumber();
+
+                    // increment clicks
+                    clicks++;
+
+                    // set text of third labels
+                    pHand3.Text = pHandNext.ToString();
+                    dHand3.Text = dHandNext.ToString();
+
+                    pHandOld = pHandOld + pHandNext;
+                    dHandOld = dHandOld + dHandNext;
+
+                    compareHands(pHandOld, dHandOld);
+
+                    break;
+
+                case 3:
+                    // get random number
+                    pHandNext = cardNumber();
+                    dHandNext = cardNumber();
+
+                    // increment clicks
+                    clicks++;
+
+                    // set text of four labels
+                    pHand4.Text = pHandNext.ToString();
+                    dHand4.Text = dHandNext.ToString();
+
+                    pHandOld = pHandOld + pHandNext;
+                    dHandOld = dHandOld + dHandNext;
+
+                    compareHands(pHandOld, dHandOld);
+
+                    break;
+                default:
+                    MessageBox.Show("More than 4 cards required.", "Limit");
+                    break;
             }
         }
+
+        // Fold button to lose game and reset
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("You lost!", "Loser!!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            if (res == DialogResult.OK)
+            {
+                bal = gambleLose(gambleAmount);
+                MessageBox.Show("New balance: " + bal, "Balance");
+
+                button2.Enabled = true;
+                button3.Enabled = false;
+                button4.Enabled = false;
+            }
+        }
+
     }
 }
